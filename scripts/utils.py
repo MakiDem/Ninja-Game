bb325ec7c7c273a6b739ac1306dc1056b50f23f4
Maki <mcjohndraycabral@gmail.com>
2025-01-25 18:29:34 +0800

    fixed colorkey for run animation using convert method

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,7 +4,7 @@ import os
 BASE_IMG_PATH = 'data/images/'
 
 def load_image(path):
-    img = pygame.image.load(BASE_IMG_PATH+path)
+    img = pygame.image.load(BASE_IMG_PATH+path).convert()
     img.set_colorkey((0,0,0))
     return img
 
