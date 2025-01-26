f9e8d78e61139e1e046c1c99399f07b68e97cd40
Maki <mcjohndraycabral@gmail.com>
2025-01-26 17:57:43 +0800

    code for level editor is started and in progress

--- editor.py
+++ editor.py
@@ -3,18 +3,17 @@ from sys import exit
 
 from pygame import Surface
 
-from scripts.entity import Entity, Player
-from scripts.utils import load_image, load_image_list, Animation
+from scripts.utils import load_image, load_image_list
 from scripts.tilemap import TileMap
 from scripts.clouds import Clouds
 
 
-class Game:
+class Editor:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((640, 480))
         self.display = Surface((320, 240))
-        pygame.display.set_caption('Ninja Game')
+        pygame.display.set_caption('Level Editor')
 
         self.assets = {
             'decor': load_image_list('tiles/decor'),
@@ -22,44 +21,31 @@ class Game:
             'large_decor': load_image_list('tiles/large_decor'),
             'spawners': load_image_list('tiles/spawners'),
             'stone': load_image_list('tiles/stone'),
-            'player': load_image('entities/player.png').convert(),
             'background': load_image('background.png'),
             'clouds': load_image_list('clouds'),
-
-            'player/idle': Animation(load_image_list('entities/player/idle'), frame_dur=4),
-            'player/run': Animation(load_image_list('entities/player/run'), frame_dur=4),
-            'player/slide': Animation(load_image_list('entities/player/slide'), frame_dur=6),
-            'player/jump': Animation(load_image_list('entities/player/jump'), frame_dur=4),
-            'player/wall-slide': Animation(load_image_list('entities/player/idle'), frame_dur=4)
         }
 
         self.clock = pygame.time.Clock()
-        self.player = Player(self, (50,50), (8,15))
-        self.movement = [False, False]
+        self.movement = [False, False, False, False]
 
         self.tile_map = TileMap(self, tile_size=16)
         self.scroll = [0,0]
 
-        self.clouds = Clouds(self.assets['clouds'])
+        self.tile_list = list(self.assets)
+        self.tile_group = 0
+        self.tile_variant = 0
+
+        self.clicking = False
+        self.right_click = False
 
     def run(self):
         while True:
-            self.display.blit(self.assets['background'], (0,0))
+            self.display.fill((0,0,0))
 
+            curr_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
+            curr_tile_img.set_alpha(100) #make img semi-transparent
 
-            self.scroll[0] += (self.player.rect().centerx - self.display.get_width()/ 2 - self.scroll[0]) / 30
-            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
-            render_offset = [int(self.scroll[0]), int(self.scroll[1])]
-
-            self.clouds.update()
-            self.clouds.render(self.display, offset=render_offset)
-
-            self.player.update(self.tile_map, (self.movement[1] - self.movement[0], 0))
-            self.player.render(offset=render_offset)
-
-            self.tile_map.render(self.display, render_offset)
-
-
+            self.display.blit(curr_tile_img, (5,5))
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0,0))
 
@@ -71,18 +57,37 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1: #left mouse button
+                        clicking = True
+                        print('left click')
+                    if event.button == 2: #mouse scroll button
+                        clicking = True
+                        print('scroll click')
+                    if event.button == 3: #right mouse button
+                        clicking = True
+                        print('right click')
+
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.movement[2] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[3] = True
 
-                    if event.key == pygame.K_SPACE:
-                        self.player.velocity[1] = -3
+
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
+                    if event.key == pygame.K_UP:
+                        self.movement[2] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[3] = False
 
-Game().run()
+Editor().run()
